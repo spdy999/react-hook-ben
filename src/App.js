2828fe6d9ab892323232756d9579aaaae5d5102b
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-17 01:32:21 +0700

    useRef()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import { useForm } from './useForm';
-import { Hello } from './Hello';
 import { useFetch } from './useFetch';
 
 const App = () => {
   const [values, handleChange] = useForm({ email: '', password: '', firstName: '' });
   const [count, setCount] = useState(() => JSON.parse(localStorage.getItem('count')));
   const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+  const inputRef = useRef();
 
   useEffect(() => {
     localStorage.setItem('count', JSON.stringify(count));
@@ -19,9 +19,17 @@ const App = () => {
       <div>count: {count}</div>
       <button onClick={() => setCount(c => c + 1)}>increment</button>
       <>
-        <input name="email" value={values.email} onChange={handleChange} />
+        <input name="email" ref={inputRef} value={values.email} onChange={handleChange} />
         <input name="firstName" value={values.firstName} onChange={handleChange} placeholder="firstName" />
         <input type="password" name="password" value={values.password} onChange={handleChange} />
+        <button
+          onClick={() => {
+            console.log(inputRef);
+            inputRef.current.focus();
+          }}
+        >
+          focus
+        </button>
       </>
     </div>
   );
