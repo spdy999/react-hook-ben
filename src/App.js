8282e33593407dc8e7db2e1f9da6aba7394d6d14
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-16 16:34:32 +0700

    separate useState

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(20);
   return (
     <div>
-      <button onClick={() => setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))}>+</button>
+      <button
+        onClick={() => {
+          setCount(c => c + 1);
+          setCount2(c => c + 1);
+        }}
+      >
+        +
+      </button>
       <div>count 1: {count}</div>
       <div>count 2: {count2}</div>
     </div>
