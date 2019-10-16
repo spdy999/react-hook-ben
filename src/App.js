ee7834ce7f278786cedc041177b3090dacce7e41
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-16 17:14:23 +0700

    multiple useEffect()

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { useForm } from './useForm';
 import { Hello } from './Hello';
 
 const App = () => {
   const [values, handleChange] = useForm({ email: '', password: '', firstName: '' });
-  const [showHello, setShowHello] = useState(true);
+  // const [showHello, setShowHello] = useState(true);
+
+  // useEffect(() => {
+  //   console.log('je;;p');
+  //   const onMouseMove = e => {
+  //     console.log(e);
+  //   };
+  //   window.addEventListener('mousemove', onMouseMove);
+  //   return () => {
+  //     window.removeEventListener('mousemove', onMouseMove);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    console.log('mount1');
+  }, []);
+  useEffect(() => {
+    console.log('mount2');
+  }, []);
   return (
     <div>
       <>
-        <button onClick={() => setShowHello(!showHello)}>toggle</button>
-        {showHello && <Hello />}
+        {/* <button onClick={() => setShowHello(!showHello)}>toggle</button>
+        {showHello && <Hello />} */}
         <input name="email" value={values.email} onChange={handleChange} />
         <input name="firstName" value={values.firstName} onChange={handleChange} placeholder="firstName" />
         <input type="password" name="password" value={values.password} onChange={handleChange} />
