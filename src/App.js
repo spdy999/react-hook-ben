ea3863abc6f005d7322e297964b002a38586e810
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-16 15:39:31 +0700

    set multiple values in one useState

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [count, setCount] = useState(5);
+  const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <div>{count}</div>
+      <button onClick={() => setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))}>+</button>
+      <div>count 1: {count}</div>
+      <div>count 2: {count2}</div>
     </div>
   );
 };
