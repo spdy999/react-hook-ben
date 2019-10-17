7248c192e5c5f01a909d2c1c7f154139536dbf43
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-17 19:49:07 +0700

    clean everything before start useCallback()

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import React, { useState, useRef } from 'react';
-import { useForm } from './useForm';
+import React from 'react';
 import { Hello } from './Hello';
 
 const App = () => {
-  const [values, handleChange] = useForm({ email: '', password: '', firstName: '' });
-
-  const inputRef = useRef();
-  const hello = useRef(() => console.log('hello  '));
-
-  const [showHello, setShowHello] = useState(true);
-
-  // useLayoutEffect(() => {
-  //   console.log(inputRef.current.getBoundingClientRect());
-  //   // return () => {
-  //   //   cleanup;
-  //   // };
-  // }, []);
-
   return (
     <div>
-      <>
-        <button onClick={() => setShowHello(!showHello)}>toggles</button>
-        {showHello && <Hello />}
-        <input name="email" ref={inputRef} value={values.email} onChange={handleChange} />
-        <input name="firstName" value={values.firstName} onChange={handleChange} placeholder="firstName" />
-        <input type="password" name="password" value={values.password} onChange={handleChange} />
-        <button
-          onClick={() => {
-            inputRef.current.focus();
-            hello.current();
-          }}
-        >
-          focus
-        </button>
-      </>
+      <Hello />
     </div>
   );
 };
--- src/Hello.js
+++ src/Hello.js
@@ -1,25 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useFetch } from './useFetch';
-import { useMesure } from './useMeasure';
+import React from 'react';
 
 export const Hello = () => {
-  const [count, setCount] = useState(() => JSON.parse(localStorage.getItem('count')));
-  const { data } = useFetch(`http://numbersapi.com/${count}/trivia`);
-
-  useEffect(() => {
-    localStorage.setItem('count', Math.floor(Math.random() * 100));
-  }, [count]);
-
-  const [rect, divRef] = useMesure([data]);
-
-  return (
-    <div>
-      <div style={{ display: 'flex' }}>
-        <div ref={divRef}>{!data ? 'loading' : data}</div>
-      </div>
-      <pre>{JSON.stringify(rect, null, 2)}</pre>
-      <div>count: {count}</div>
-      <button onClick={() => setCount(c => c + 1)}>increment</button>
-    </div>
-  );
+  return <div>Hello</div>;
 };
